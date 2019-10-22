7abd6174c8a8bbe3c6951dfa7a3177174eba0e85
Danyal Ahmad <danyal20098@gmail.com>
2019-10-22 15:04:51 +0500

    installing chaincode script  updated

--- fabric/first-network/installChaincode.sh
+++ fabric/first-network/installChaincode.sh
@@ -15,3 +15,9 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_TLS_ROOTCERT_FILE=/o
 export ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
 docker exec cli peer chaincode instantiate -o orderer.example.com:7050 --cafile $ORDERER_CA -C mychannel -c '{"Args":[]}' -n certificate -v 1.0.1 -P "OR('Org1MSP.member', 'Org2MSP.member')"
+
+sleep 3s
+docker exec cli peer chaincode invoke -o orderer.example.com:7050 --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n certificate --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt -c '{"Args":["addCertificate","01","university-1","GIK","student-1","Inshar","#$%#FFDVV$@DWCEFV%#$","2018-10-21T00:00:00Z"]}'
+
+sleep 3s
+docker exec cli peer chaincode invoke -o orderer.example.com:7050 --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n certificate --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt -c '{"Args":["queryCertificate","#$%#FFDVV$@DWCEFV%#$"]}'
