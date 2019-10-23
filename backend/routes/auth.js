e1d894d3c52683bc17bf31e5b4f76c3d8271ddc2
Danyal Ahmad <danyal20098@gmail.com>
2019-10-23 21:24:00 +0500

    removed avatar

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const AuthController = require("../controllers/auth");
-const avatar = require("../middleware/avatar");
 const authorize = require("../middleware/authorize");
 
 
 // Login
 router.post("/login", AuthController.login);
-router.post("/register", avatar, AuthController.register);
+router.post("/register", AuthController.register);
 router.post("/social/login",AuthController.socialLogin);
 router.post("/reset/request",AuthController.resetRequest);
 router.post("/reset/password",AuthController.resetPassword);
 router.post("/change/password", authorize, AuthController.changePassword);
-router.post("/profile/update", avatar, authorize, AuthController.update);
+router.post("/profile/update", authorize, AuthController.update);
 
 module.exports = router;
