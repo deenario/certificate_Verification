5cbbe9c2c156db3e7f9b384ba2069ef2fd8e5e69
Danyal Ahmad <danyal20098@gmail.com>
2019-10-24 15:46:18 +0500

    auth routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const AuthController = require("../controllers/auth");
 const authorize = require("../middleware/authorize");
 
-
 // Login
 router.post("/login", AuthController.login);
 router.post("/register", AuthController.register);
-router.post("/social/login",AuthController.socialLogin);
 router.post("/reset/request",AuthController.resetRequest);
 router.post("/reset/password",AuthController.resetPassword);
 router.post("/change/password", authorize, AuthController.changePassword);
