57af7cd21f63c2792ab8ec15273d9b0faec13486
Danyal Ahmad <danyal20098@gmail.com>
2019-10-28 14:33:37 +0500

    api

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -25,7 +25,7 @@ let readHTMLFile = function (path, callback) {
 };
 
 let transporter = nodemailer.createTransport({
-    host: "Gmail",
+    host: "smtp.gmail.com",
     auth: {
         user: "blockchain.certification.lk@gmail.com",
         pass: "Abc@1234"
